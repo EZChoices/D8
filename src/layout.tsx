376bc9a52e0939e8559a04aa90b01e4e780e2da8
EZChoices <MaanHilal@gmail.com>
2025-09-10 08:37:23 -0700

    chore: remove duplicate layout file

--- src/layout.tsx
+++
@@ -1,9 +0,0 @@
-export const metadata = { title: "D8" };
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
