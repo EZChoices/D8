521f0226efc0714664d2560c977f684e7fe591e8
EZChoices <MaanHilal@gmail.com>
2025-09-10 08:10:03 -0700

    chore: configure next app

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // DO NOT use experimental.appDir — it was removed in newer Next.
 };
 
 module.exports = nextConfig;
