b57342daf51f2e2cf291f43e58483d3ac1ac8ae5
EZChoices <MaanHilal@gmail.com>
2025-09-10 16:59:31 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // DO NOT use experimental.appDir — it was removed in newer Next.
 };
 
 module.exports = nextConfig;
